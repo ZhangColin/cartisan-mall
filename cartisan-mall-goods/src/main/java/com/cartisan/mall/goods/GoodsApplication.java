e6f9a33e954383f54a6f219b170b44fb93a4fde1
张锦华 <colin.zhang@zsgcm.com>
2021-10-10 22:00:42 +0800

    add goods-brand db create script

---
+++ cartisan-mall-goods/src/main/java/com/cartisan/mall/goods/GoodsApplication.java
@@ -0,0 +1,26 @@
+package com.cartisan.mall.goods;
+
+import com.cartisan.utils.SnowflakeIdWorker;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author colin
+ */
+@SpringBootApplication(scanBasePackages = {"com.cartisan", "com.cartisan.mall"})
+@MapperScan("com.cartisan.mall.**.mapper")
+@Slf4j
+public class GoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+
+    @Bean
+    public SnowflakeIdWorker idWorker() {
+        return new SnowflakeIdWorker(1, 1);
+    }
+
+}
---
+++ cartisan-mall-goods/src/main/resources/db/migration/V0001__create_brand_table.sql
@@ -0,0 +1,13 @@
+-- 品牌表
+DROP TABLE IF EXISTS `gds_brands`;
+
+CREATE TABLE `gds_brands` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '品牌id',
+    `name` varchar(100) NOT NULL COMMENT '品牌名称',
+    `image` varchar(1000) DEFAULT '' COMMENT '品牌图片地址',
+    `letter` char(1) DEFAULT '' COMMENT '品牌的首字母',
+    `sequence` int NOT NULL DEFAULT 0 COMMENT '排序',
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='品牌表';
