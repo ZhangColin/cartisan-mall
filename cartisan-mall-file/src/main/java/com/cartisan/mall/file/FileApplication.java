1c143f36e7553de8c1d0517466b287e0a33dc108
张锦华 <colin.zhang@zsgcm.com>
2021-10-14 21:46:55 +0800

    file upload and admin project

--- cartisan-mall-file/src/main/java/com/cartisan/mall/file/FileApplication.java
+++ cartisan-mall-file/src/main/java/com/cartisan/mall/file/FileApplication.java
@@ -1,7 +1,9 @@
 package com.cartisan.mall.file;
 
+import com.cartisan.util.SnowflakeIdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author colin
@@ -10,5 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FileApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class);
+    }
+
+    @Bean
+    public SnowflakeIdWorker snowflakeIdWorker() {
+        return new SnowflakeIdWorker(0, 0);
     }
 }
--- cartisan-mall-file/src/main/java/com/cartisan/mall/file/controller/FileController.java
+++ cartisan-mall-file/src/main/java/com/cartisan/mall/file/controller/FileController.java
@@ -1,31 +1,95 @@
 package com.cartisan.mall.file.controller;
 
-import com.cartisan.mall.file.FastDFSFile;
-import com.cartisan.mall.file.util.FastDFSClient;
-import org.springframework.util.StringUtils;
+import com.cartisan.util.SnowflakeIdWorker;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.errors.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.cartisan.response.ResponseUtil.success;
+import static com.cartisan.response.ResponseUtil.successWithMessage;
 
 /**
  * @author colin
  */
 @RestController
 public class FileController {
-    @PostMapping("/upload")
-    public String upload(@RequestParam(value = "file") MultipartFile file) {
-        try {
-            final FastDFSFile fastDFSFile = new FastDFSFile(file.getOriginalFilename(), file.getBytes(),
-                    StringUtils.getFilenameExtension(file.getOriginalFilename()));
-            final String[] upload = FastDFSClient.upload(fastDFSFile);
+    private final SnowflakeIdWorker idWorker;
 
-            return FastDFSClient.getTrackerUrl()+"/"+upload[0]+"/"+upload[1];
-        } catch (IOException e) {
-        }
-
-        return null;
+    public FileController(SnowflakeIdWorker idWorker) {
+        this.idWorker = idWorker;
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Map<String, String>> upload(@RequestParam(value = "file") MultipartFile file)
+            throws ServerException, InsufficientDataException,
+            ErrorResponseException, IOException, NoSuchAlgorithmException,
+            InvalidKeyException, InvalidResponseException, XmlParserException,
+            InternalException {
+
+        final MinioClient minioClient = MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("zhang_colin", "cartisan")
+                .build();
+
+//        final boolean exists = minioClient.bucketExists(
+//                BucketExistsArgs.builder().bucket("cartisan-mall").build());
+//
+//        if (!exists) {
+//            minioClient.makeBucket(MakeBucketArgs.builder().bucket("cartisan-mall").build());
+//        }
+
+        final String filename = file.getOriginalFilename();
+
+        final String objectName = String.format("%s/%s", idWorker.nextId(), filename);
+
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket("cartisan-mall")
+                .contentType(file.getContentType())
+                .object(objectName)
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .build());
+
+        final HashMap<String, String> map = new HashMap<String, String>();
+        map.put("filename", filename);
+        map.put("url", "http://localhost:9000/cartisan-mall/" + objectName);
+
+        return success(map);
+
+    }
+
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam(value = "objectName") String objectName)
+            throws ServerException, InsufficientDataException,
+            ErrorResponseException, IOException, NoSuchAlgorithmException,
+            InvalidKeyException, InvalidResponseException, XmlParserException,
+            InternalException {
+
+        final MinioClient minioClient = MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("zhang_colin", "cartisan")
+                .build();
+
+        minioClient.removeObject(RemoveObjectArgs.builder()
+                .bucket("cartisan-mall")
+                .object(objectName)
+                .build());
+
+        return successWithMessage("删除成功。");
+
+    }
+
+
 }
