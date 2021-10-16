a96f7b6638e7acaf2d574f79fca3cada04911f2a
张锦华 <colin.zhang@zsgcm.com>
2021-10-16 15:55:31 +0800

    add goods db script

--- cartisan-mall-goods/src/main/resources/db/dev/migration/R__add_template_data.sql
+++ cartisan-mall-goods/src/main/resources/db/dev/migration/R__add_template_data.sql
@@ -1,25 +1,25 @@
 -- 模板表
 truncate `gds_templates`;
 
-INSERT INTO `gds_templates`(`id`,`name`) VALUES (42, '手机');
-INSERT INTO `gds_templates`(`id`,`name`) VALUES (43, '电视');
+INSERT INTO gds_templates (id, name) VALUES (42, '手机');
+INSERT INTO gds_templates (id, name) VALUES (43, '电视');
 
 -- 规格表
 truncate `gds_specifications`;
 
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (26, '尺码', '165,170,175', 43);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (27, '网络', '联通2G,联通3G,联通4G,移动4G,联通3G/移动4G,电信4G', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (28, '手机屏幕尺寸', '5寸,5.5寸', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (32, '机身内存', '16G,32G,128G', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (33, '电视屏幕尺寸', '20英寸,50英寸,60英寸', 43);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (34, '存储', '16G,32G,64G', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (36, '像素', '300万像素,800万像素', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (37, '电视音响效果', '立体声,环绕,小影院', 43);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (38, '颜色', '红,绿,蓝,紫,白,黑', 42);
-INSERT INTO `gds_specifications`(`id`,`name`,`options`,`template_id`) VALUES (39, '测试', '实施,学习,实施,测试,显示,s11', 42);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (26, 43, '尺码', '165,170,175', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (27, 42, '网络', '联通2G,联通3G,联通4G,移动4G,联通3G/移动4G,电信4G', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (28, 42, '手机屏幕尺寸', '5寸,5.5寸', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (32, 42, '机身内存', '16G,32G,128G', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (33, 43, '电视屏幕尺寸', '20英寸,50英寸,60英寸', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (34, 42, '存储', '16G,32G,64G', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (36, 42, '像素', '300万像素,800万像素', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (37, 43, '电视音响效果', '立体声,环绕,小影院', 1);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (38, 42, '颜色', '红,绿,蓝,紫,白,黑', 11);
+INSERT INTO gds_specifications (id, template_id, name, options, sequence) VALUES (39, 42, '测试', '实施,学习,实施,测试,显示,s11', 1);
 
 -- 参数表
 truncate `gds_parameters`;
 
-INSERT INTO `gds_parameters`(`id`,`name`,`options`,`template_id`) VALUES (1, '出厂年份', '2001,2002,2004,2005', 42);
-INSERT INTO `gds_parameters`(`id`,`name`,`options`,`template_id`) VALUES (2, '版本', '10,20,30', 42);
+INSERT INTO gds_parameters (id, template_id, name, options, sequence) VALUES (1, 42, '出厂年份', '2001,2002,2004,2005', 1);
+INSERT INTO gds_parameters (id, template_id, name, options, sequence) VALUES (2, 42, '版本', '10,20,30', 11);
---
+++ cartisan-mall-goods/src/main/resources/db/migration/V0006__create_goods_tables.sql
@@ -0,0 +1,49 @@
+-- SPU表
+DROP TABLE IF EXISTS `gds_spus`;
+
+CREATE TABLE `gds_spus` (
+    `id` bigint NOT NULL COMMENT 'SPUid',
+    `sn` varchar(60) NOT NULL COMMENT '货号',
+    `name` varchar(100) NOT NULL COMMENT '名称',
+    `caption` varchar(100) NOT NULL COMMENT '副标题',
+    `brand_id` bigint NULL COMMENT '品牌Id',
+    `category1_id` bigint NULL COMMENT '一级分类Id',
+    `category2_id` bigint NULL COMMENT '二级分类Id',
+    `category3_id` bigint NULL COMMENT '三级分类Id',
+    `template_id` bigint NULL COMMENT '模板Id',
+    `freight_id` bigint NULL COMMENT '运费模板Id',
+    `image` varchar(256) DEFAULT '' COMMENT '图片',
+    `images` varchar(1000) DEFAULT '' COMMENT '图片列表',
+    `sale_service` varchar(50) DEFAULT '' COMMENT '售后服务',
+    `introduction` text NULL COMMENT '介绍',
+    `specification_items` varchar(3000) DEFAULT '' COMMENT '规格',
+    `parameter_items` varchar(3000) DEFAULT '' COMMENT '参数',
+    `is_marketable` bit(1) NOT NULL DEFAULT b'1' COMMENT '是否上架',
+    `enable_specification` bit(1) NOT NULL DEFAULT b'1' COMMENT '是否启用规格',
+    `audit_status` tinyint NOT NULL DEFAULT 1 COMMENT '审核状态',
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `active` bit(1) NOT NULL DEFAULT b'1',
+    `deleted` bit(1) NOT NULL DEFAULT b'0',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='SPU';
+
+-- SKU表
+DROP TABLE IF EXISTS `gds_skus`;
+
+CREATE TABLE `gds_skus` (
+    `id` bigint NOT NULL COMMENT 'SKUid',
+    `spu_id` bigint NULL COMMENT 'SPUId',
+    `sn` varchar(60) NOT NULL COMMENT '商品条码',
+    `name` varchar(100) NOT NULL COMMENT '名称',
+    `specification` varchar(1000) DEFAULT '' COMMENT '规格',
+    `price` int NOT NULL COMMENT '价格（分）',
+    `weight` int NOT NULL COMMENT '重量（克）',
+    `quantity` int NOT NULL COMMENT '库存数量',
+    `alert_quantity` int NOT NULL COMMENT '库存预警数量',
+    `image` varchar(256) DEFAULT '' COMMENT '商品图片',
+    `images` varchar(1000) DEFAULT '' COMMENT '商品图片列表',
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='SKU';
